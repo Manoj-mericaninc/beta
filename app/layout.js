e41f50445cf757f7b2717ceffaf23a9482c515f7
Kishore <kishore.sai37@gmail.com>
2023-06-15 15:39:30 +0530

    update template files

---
+++ app/layout.js
@@ -0,0 +1,21 @@
+import '@/styles/globals.css';
+
+export const metadata = {
+  title: 'Zen Time',
+  description: 'Zen Time application to manage time sheets',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <head>
+        <link
+          rel='shortcut icon'
+          href='/assets/images/favicon.jpeg'
+          type='image/x-icon'
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
