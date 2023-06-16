07dd101ed05d721f581ce82542cc84f28b31f450
Manoj Kumar <manoj@mericaninc.com>
2023-06-16 11:54:56 +0530

    Added old files

---
+++ app/SubmenuContents/Accounts/page.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const Accounts = () => {
+  return (
+    <div className="flex h-screen bg-green-300 justify-center items-center text-lg font-semibold">
+      Accounts
+    </div>
+  );
+};
+
+export default Accounts;
---
+++ app/SubmenuContents/Business/page.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex h-screen bg-orange-300 justify-center items-center text-lg font-semibold">
+      <h1>Business Information</h1>
+    </div>
+  );
+};
+
+export default page;
