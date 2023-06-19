8ad9d394fd49e0aace4b6a6a898098bba3a4cd89
Kishore <kishore.sai37@gmail.com>
2023-06-19 16:53:03 +0530

    update page files

--- app/page.js
+++ app/page.js
@@ -1,9 +1,5 @@
-import Employeetables from "@/components/employeTable";
-
 const page = () => {
-  return <div>
-    <Employeetables/>
-  </div>;
+  return <div>page</div>;
 };
 
 export default page;
