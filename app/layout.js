e11dd6484e41622fded75d372728dae1395c80e0
Kishore <kishore.sai37@gmail.com>
2023-06-19 16:54:35 +0530

    updated styles link in layout

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,22 @@
-import { TableContextProvider } from "@/components/tableContext/tableContext";
-import "@styles/global.css";
+import { TableContextProvider } from '@/components/tableContext/tableContext';
+import '@/styles/global.css';
 export const metadata = {
-  title: "Zen Time",
-  description: "Zen Time application to manage time sheets",
+  title: 'Zen Time',
+  description: 'Zen Time application to manage time sheets',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
         <link
-          rel="shortcut icon"
-          href="/assets/images/favicon.jpeg"
-          type="image/x-icon"
+          rel='shortcut icon'
+          href='/assets/images/favicon.jpeg'
+          type='image/x-icon'
         />
       </head>
       <body>
-        {" "}
+        {' '}
         <TableContextProvider>{children}</TableContextProvider>
       </body>
     </html>
